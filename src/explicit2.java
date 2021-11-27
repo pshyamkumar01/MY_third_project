8478c1e400531a1f1f7c4f93654495d87bf135b9
pshyamkumar01 <63715715+pshyamkumar01@users.noreply.github.com>
2021-11-27 14:42:05 +0530

    completed explicit coding

--- src/explicit2.java
+++ src/explicit2.java
@@ -4,5 +4,11 @@ public class explicit2
 	public static void main(String args[])
 	{
 		System.out.println("WELCOME TO EXPLICIT_CASTING");
+		
+		char c = 'z';
+		short s = (short)c;
+		
+		byte b = (byte)s;
+		System.out.println(b);
 	}
 }
